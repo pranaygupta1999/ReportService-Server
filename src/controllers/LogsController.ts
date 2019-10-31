eddcc542ffa0cd8dcbcac96c83a3cfbcf6e807fc
Pranay Gupta <pranaygupta1999@gmail.com>
2019-10-31 15:20:11 +0530

    Added controllers | SQL connection | Initial commit

---
+++ src/controllers/LogsController.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from 'express' ;
+import connection  from '../db';
+import {MysqlError, FieldInfo} from 'mysql'
+
+var LogControllers =  {
+    getAllLogs: function(req: Request, res: Response){
+        connection.query("Select * from Logs", (err, rows, field)=>{
+            res.json(rows);
+        })
+    },
+    getLog: function(req: Request, res: Response){
+        connection.query(`SELECT * FROM Logs WHERE lid= ${req.params['lid']}`, (err, rows, field)=>{
+            if(!err)
+                res.json(rows);
+            else
+                res.status(500).json({msg: 'Could not find the log', err: err})
+        })
+    }
+}
+export default LogControllers;

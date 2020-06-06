8a180f7db7c33c59ad1f251e04e4f0f459747af5
Pranay Gupta <pranaygupta1999@gmail.com>
2020-06-06 11:49:24 +0530

    add env variables for host and db name

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,8 @@
 import mysql, { Connection } from 'mysql';
 const connection: Connection = mysql.createConnection({
     user: process.env.MYSQL_USER ? process.env.MYSQL_USER : 'root',
-    password: process.env.MYSQL_PASSWORD ? process.env.MYSQL_PASSWORD : "",
+    host: process.env.MYSQL_HOST ? process.env.MYSQL_HOST : 'localhost',
+    password: process.env.MYSQL_PASSWORD ? process.env.MYSQL_PASSWORD : "tiger",
     database: process.env.MYSQL_DATABASE ? process.env.MYSQL_DATABASE : "ReportService"
 });
 connection.connect((err) => {
@@ -9,3 +10,4 @@ connection.connect((err) => {
     else console.log("Logs: \x1b[32mConnection to Mysql established\x1b[0m");
 });
 export default connection;
+//some change
